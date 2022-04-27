c483b7778b08b59f8cdbd7ce712d71f9f9501244
ganugulateja <varains2133@gmail.com>
2022-04-27 19:42:33 +0530

    updated

---
+++ script.sh
@@ -0,0 +1,7 @@
+#! /bin/bash 
+echo "**************************before reboot*************************** "
+uname -a
+df -h
+netstat -nr
+uptime
+echo " ******************prechecks execution completed***************** "
