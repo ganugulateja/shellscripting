9ddd84e1529061d25c47916ff2fae26f0cce911f
ganugulateja <varains2133@gmail.com>
2022-05-02 00:37:52 +0530

    update2

---
+++ arg.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo " Argument total: $# "
+echo " script name: $0"
+echo " script 1st Argument: $1 "
+echo " script 1st Argument: $2 "
+echo " script 1st Argument: $3 "
+echo " list of all Arguments: * $* "
+echo " lsit of all Arguments: @ $@ "
+echo " PID of shell: $$ "
